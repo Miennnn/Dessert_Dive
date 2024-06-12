248cc65b17e48305f2e23859a00f5bb441f5d009
Miennnn <e1122358@u.nus.edu>
2024-06-12 11:17:32 +0800

    Bug: Skips login page and goes into homscreeen. Aim is Login.tsx to HomeScreen.tsx without _layout layer if possible

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
-
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Layout from './app/_layout';
+import { createStackNavigator } from '@react-navigation/stack';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { FIREBASE_AUTH } from './FirebaseConfig';
+import Login from './app/Login';
+import HomeScreen from '.app/HomeScreen';
+import DessertsNearMe from './app/screens/DessertsNearMe';
+import Recipes from './app/screens/Recipes';
+import IngredientsList from './app/screens/IngredientsList';
+import Questionnaire from './app/screens/Questionnaire';
+import Account from './app/screens/Account';
+
+const Stack = createStackNavigator();
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
@@ -16,7 +26,20 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Layout />
+      <Stack.Navigator initialRouteName="Login">
+        {user ? (
+          <>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="DessertsNearMe" component={DessertsNearMe} />
+            <Stack.Screen name="Recipes" component={Recipes} />
+            <Stack.Screen name="IngredientsList" component={IngredientsList} />
+            <Stack.Screen name="Questionnaire" component={Questionnaire} />
+            <Stack.Screen name="Account" component={Account} />
+          </>
+        ) : (
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        )}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ FirebaseConfig.tsx
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZh1433bTr1HSfs_eDPoN3h1NmA89UtEk",
+  authDomain: "dessertdive-66eec.firebaseapp.com",
+  projectId: "dessertdive-66eec",
+  storageBucket: "dessertdive-66eec.appspot.com",
+  messagingSenderId: "225188201884",
+  appId: "1:225188201884:web:66903b5999ea5cbf347459",
+  measurementId: "G-DL0SLV0M09"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
