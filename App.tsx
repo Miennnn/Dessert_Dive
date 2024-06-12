720bd275359bc85aff7da58ce85c1882fc3fe6f0
Miennnn <e1122358@u.nus.edu>
2024-06-12 15:38:42 +0800

    Revert "Reapply "Reapply "Bug Update: Chosen to keep Layout screen, but now it starts straight in the homescreen and not from login screen"""
    This reverts commit a4e2156e3e7c1809c3790872dd0a726f071f4765.

--- App.tsx
+++ App.tsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FirebaseConfig';
-import Login from './app/screens/Login';
-import Layout from './app/_layout';
+import Login from './app/Login';
+import HomeScreen from '.app/HomeScreen';
+import DessertsNearMe from './app/screens/DessertsNearMe';
+import Recipes from './app/screens/Recipes';
+import IngredientsList from './app/screens/IngredientsList';
+import Questionnaire from './app/screens/Questionnaire';
+import Account from './app/screens/Account';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
       console.log('user', user);
       setUser(user);
     });
 
-    return () => unsubscribe();
   }, []);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         {user ? (
-          <Stack.Screen name="Layout" component={Layout}  />
+          <>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="DessertsNearMe" component={DessertsNearMe} />
+            <Stack.Screen name="Recipes" component={Recipes} />
+            <Stack.Screen name="IngredientsList" component={IngredientsList} />
+            <Stack.Screen name="Questionnaire" component={Questionnaire} />
+            <Stack.Screen name="Account" component={Account} />
+          </>
         ) : (
-          <Stack.Screen name="Login" component={Login} />
-
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
